c30237903d4b28e5461057960b11c8505520f6db
Yukino-hub <samu31.h0@gmail.com>
2026-02-03 17:11:06 +0800

    Update CORS origin to new domain

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://yukino-hub.github.io', 
+  origin: 'https://kouritensg.github.io', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
